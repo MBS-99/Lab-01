7ffd825a1bf3a28e213ccadd585fbfd11f9ea928
banysaleh99 <banysaleh99@gmail.com>
2022-08-26 20:24:03 +0300

    first update

---
+++ main.dart
@@ -0,0 +1,20 @@
+void main() {
+  Map mohammadInformation = {
+    "First Name": "Mohammad",
+    "Last Name": "Bany Saleh",
+    "Age": 23,
+    "Education Info": {
+      "University name": "Yarmouk University",
+      "Faculty": "Information Technology and Computer Sience",
+      "Graduation Year": 2022,
+    },
+    "Hobbies": [
+      {
+        "Sports": ["Football", "Tinnes"],
+        "Other": ["Reading", "Traveling"]
+      }
+    ]
+  };
+
+  print(mohammadInformation);
+}
